2f5d7f97d812fa033a810b32f82d8d7de09f0090
Soufiene NASRI <snasri@squad.fr>
2022-02-28 09:14:06 +0100

    1

--- client-country/src/main/java/overview/PieFuelConso.java
+++ client-country/src/main/java/overview/PieFuelConso.java
@@ -1,42 +1,299 @@
 package overview;
 
-public class PieFuelConso {
+public class FuelConso {
+    private String fuelName1;
+    private Integer blFuelConso1;
+    private Integer optFuelConso1;
+    private String fuelColor1;
 
-    private String keyFuel;
-    private Integer consoFuel;
-    private String nameFuel;
-    private String colorFuel1;
+    private String fuelName2;
+    private Integer blFuelConso2;
+    private Integer optFuelConso2;
+    private String fuelColor2;
 
+    private String fuelName3;
+    private Integer blFuelConso3;
+    private Integer optFuelConso3;
+    private String fuelColor3;
 
-    public String getKeyFuel() {
-        return keyFuel;
+    private String fuelName4;
+    private Integer blFuelConso4;
+    private Integer optFuelConso4;
+    private String fuelColor4;
+
+    private String fuelName5;
+    private Integer blFuelConso5;
+    private Integer optFuelConso5;
+    private String fuelColor5;
+
+    private String fuelName6;
+    private Integer blFuelConso6;
+    private Integer optFuelConso6;
+    private String fuelColor6;
+
+    private String fuelName7;
+    private Integer blFuelConso7;
+    private Integer optFuelConso7;
+    private String fuelColor7;
+
+    private String fuelName8;
+    private Integer blFuelConso8;
+    private Integer optFuelConso8;
+    private String fuelColor8;
+
+    public String getFuelName1() {
+        return fuelName1;
     }
 
-    public void setKeyFuel(String keyFuel) {
-        this.keyFuel = keyFuel;
+    public void setFuelName1(String fuelName1) {
+        this.fuelName1 = fuelName1;
     }
 
-    public Integer getConsoFuel() {
-        return consoFuel;
+    public Integer getBlFuelConso1() {
+        return blFuelConso1;
     }
 
-    public void setConsoFuel(Integer consoFuel) {
-        this.consoFuel = consoFuel;
+    public void setBlFuelConso1(Integer blFuelConso1) {
+        this.blFuelConso1 = blFuelConso1;
     }
 
-    public String getNameFuel() {
-        return nameFuel;
+    public Integer getOptFuelConso1() {
+        return optFuelConso1;
     }
 
-    public void setNameFuel(String nameFuel) {
-        this.nameFuel = nameFuel;
+    public void setOptFuelConso1(Integer optFuelConso1) {
+        this.optFuelConso1 = optFuelConso1;
     }
 
-    public String getColorFuel1() {
-        return colorFuel1;
+    public String getFuelColor1() {
+        return fuelColor1;
     }
 
-    public void setColorFuel1(String colorFuel1) {
-        this.colorFuel1 = colorFuel1;
+    public void setFuelColor1(String fuelColor1) {
+        this.fuelColor1 = fuelColor1;
+    }
+
+    public String getFuelName2() {
+        return fuelName2;
+    }
+
+    public void setFuelName2(String fuelName2) {
+        this.fuelName2 = fuelName2;
+    }
+
+    public Integer getBlFuelConso2() {
+        return blFuelConso2;
+    }
+
+    public void setBlFuelConso2(Integer blFuelConso2) {
+        this.blFuelConso2 = blFuelConso2;
+    }
+
+    public Integer getOptFuelConso2() {
+        return optFuelConso2;
+    }
+
+    public void setOptFuelConso2(Integer optFuelConso2) {
+        this.optFuelConso2 = optFuelConso2;
+    }
+
+    public String getFuelColor2() {
+        return fuelColor2;
+    }
+
+    public void setFuelColor2(String fuelColor2) {
+        this.fuelColor2 = fuelColor2;
+    }
+
+    public String getFuelName3() {
+        return fuelName3;
+    }
+
+    public void setFuelName3(String fuelName3) {
+        this.fuelName3 = fuelName3;
+    }
+
+    public Integer getBlFuelConso3() {
+        return blFuelConso3;
+    }
+
+    public void setBlFuelConso3(Integer blFuelConso3) {
+        this.blFuelConso3 = blFuelConso3;
+    }
+
+    public Integer getOptFuelConso3() {
+        return optFuelConso3;
+    }
+
+    public void setOptFuelConso3(Integer optFuelConso3) {
+        this.optFuelConso3 = optFuelConso3;
+    }
+
+    public String getFuelColor3() {
+        return fuelColor3;
+    }
+
+    public void setFuelColor3(String fuelColor3) {
+        this.fuelColor3 = fuelColor3;
+    }
+
+    public String getFuelName4() {
+        return fuelName4;
+    }
+
+    public void setFuelName4(String fuelName4) {
+        this.fuelName4 = fuelName4;
+    }
+
+    public Integer getBlFuelConso4() {
+        return blFuelConso4;
+    }
+
+    public void setBlFuelConso4(Integer blFuelConso4) {
+        this.blFuelConso4 = blFuelConso4;
+    }
+
+    public Integer getOptFuelConso4() {
+        return optFuelConso4;
+    }
+
+    public void setOptFuelConso4(Integer optFuelConso4) {
+        this.optFuelConso4 = optFuelConso4;
+    }
+
+    public String getFuelColor4() {
+        return fuelColor4;
+    }
+
+    public void setFuelColor4(String fuelColor4) {
+        this.fuelColor4 = fuelColor4;
+    }
+
+    public String getFuelName5() {
+        return fuelName5;
+    }
+
+    public void setFuelName5(String fuelName5) {
+        this.fuelName5 = fuelName5;
+    }
+
+    public Integer getBlFuelConso5() {
+        return blFuelConso5;
+    }
+
+    public void setBlFuelConso5(Integer blFuelConso5) {
+        this.blFuelConso5 = blFuelConso5;
+    }
+
+    public Integer getOptFuelConso5() {
+        return optFuelConso5;
+    }
+
+    public void setOptFuelConso5(Integer optFuelConso5) {
+        this.optFuelConso5 = optFuelConso5;
+    }
+
+    public String getFuelColor5() {
+        return fuelColor5;
+    }
+
+    public void setFuelColor5(String fuelColor5) {
+        this.fuelColor5 = fuelColor5;
+    }
+
+    public String getFuelName6() {
+        return fuelName6;
+    }
+
+    public void setFuelName6(String fuelName6) {
+        this.fuelName6 = fuelName6;
+    }
+
+    public Integer getBlFuelConso6() {
+        return blFuelConso6;
+    }
+
+    public void setBlFuelConso6(Integer blFuelConso6) {
+        this.blFuelConso6 = blFuelConso6;
+    }
+
+    public Integer getOptFuelConso6() {
+        return optFuelConso6;
+    }
+
+    public void setOptFuelConso6(Integer optFuelConso6) {
+        this.optFuelConso6 = optFuelConso6;
+    }
+
+    public String getFuelColor6() {
+        return fuelColor6;
+    }
+
+    public void setFuelColor6(String fuelColor6) {
+        this.fuelColor6 = fuelColor6;
+    }
+
+    public String getFuelName7() {
+        return fuelName7;
+    }
+
+    public void setFuelName7(String fuelName7) {
+        this.fuelName7 = fuelName7;
+    }
+
+    public Integer getBlFuelConso7() {
+        return blFuelConso7;
+    }
+
+    public void setBlFuelConso7(Integer blFuelConso7) {
+        this.blFuelConso7 = blFuelConso7;
+    }
+
+    public Integer getOptFuelConso7() {
+        return optFuelConso7;
+    }
+
+    public void setOptFuelConso7(Integer optFuelConso7) {
+        this.optFuelConso7 = optFuelConso7;
+    }
+
+    public String getFuelColor7() {
+        return fuelColor7;
+    }
+
+    public void setFuelColor7(String fuelColor7) {
+        this.fuelColor7 = fuelColor7;
+    }
+
+    public String getFuelName8() {
+        return fuelName8;
+    }
+
+    public void setFuelName8(String fuelName8) {
+        this.fuelName8 = fuelName8;
+    }
+
+    public Integer getBlFuelConso8() {
+        return blFuelConso8;
+    }
+
+    public void setBlFuelConso8(Integer blFuelConso8) {
+        this.blFuelConso8 = blFuelConso8;
+    }
+
+    public Integer getOptFuelConso8() {
+        return optFuelConso8;
+    }
+
+    public void setOptFuelConso8(Integer optFuelConso8) {
+        this.optFuelConso8 = optFuelConso8;
+    }
+
+    public String getFuelColor8() {
+        return fuelColor8;
+    }
+
+    public void setFuelColor8(String fuelColor8) {
+        this.fuelColor8 = fuelColor8;
     }
 }
